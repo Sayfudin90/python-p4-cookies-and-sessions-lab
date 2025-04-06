943d23c368b5064272625f1ef92187377d07aef4
Sayfudin90 <Seifdin62@gmail.com>
2025-04-06 17:01:37 +0300

    finished lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,34 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = Article.query.all()
+    return jsonify([article.to_dict() for article in articles])
 
-    pass
+    
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize page_views if it doesn't exist in session
+    if 'page_views' not in session:
+        session['page_views'] = 0
+    
+    # Increment the page views counter
+    session['page_views'] += 1
+    
+    # Check if user has exceeded the maximum (3) page views
+    if session['page_views'] <= 3:
+        # User hasn't reached limit, serve the article
+        article = Article.query.filter_by(id=id).first()
+        
+        if article:
+            return article.to_dict()
+        else:
+            return {"message": "Article not found"}, 404
+    else:
+        # User has exceeded maximum page views, return error
+        return {"message": "Maximum pageview limit reached"}, 401
 
-    pass
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
